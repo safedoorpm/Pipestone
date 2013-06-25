30bfd97b41f6668cd2f49cb6df8d47c7c60bdc5a
daboulet <danny@savrola.com>
2013-06-24 19:18:14 -0600

    Bookman 1.1.3
    Replace de-select code in the rules spreadsheet with code that remembers the REETs which have been selected and then just de-selects them explicitly when the time comes to do a de-selection.
    
    Rework the inspection element number and name parsing code to avoid booms when validating user input when they change the number or name of an inspection element.
    
    Reduce the minimum size of most of the various Bookman windows.  Adjust the preferred size on a few to make sure that look reasonable when first opened.
    
    Rework how rules are duplicated to avoid a boom (no idea how this every worked).
    
    Add a validation method to the RSC class which checks that the rules, rule clauses and REETs in the rules in the sheet contain no duplicates (would have caught the duplication bug described above).

---
+++ src/com/obtuse/util/FormattingVector.java
@@ -0,0 +1,87 @@
+package com.obtuse.util;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * An extension of the {@link java.util.Vector} which has a toString that formats the list's contents.
+ */
+public class FormattingVector<E> extends Vector<E> {
+
+    public static final int DEFAULT_MAX_ELEMENTS_TO_FORMAT = 10;
+    public static final int DEFAULT_TAIL_ELEMENTS_TO_ALWAYS_FORMAT = 3;
+
+    private int _maxElementsToFormat = DEFAULT_MAX_ELEMENTS_TO_FORMAT;
+    private int _tailElementsToAlwaysFormat = DEFAULT_TAIL_ELEMENTS_TO_ALWAYS_FORMAT;
+
+    public FormattingVector() {
+        super();
+
+    }
+
+    public FormattingVector( java.util.Collection<? extends E> es ) {
+        super( es );
+
+    }
+
+    private StringBuilder formatNakedRange( int startIx, int endIx ) {
+
+        StringBuilder sb = new StringBuilder();
+        String comma = "";
+        for ( int ix = startIx; ix <= endIx; ix += 1 ) {
+
+            sb.append( comma ).append( get( ix ).toString() );
+
+            comma = ", ";
+
+        }
+
+        return sb;
+
+    }
+
+    public String toString() {
+
+        StringBuilder rval = new StringBuilder( "{" );
+
+        if ( !isEmpty() ) {
+
+            rval.append( " " );
+
+            if ( size() > _maxElementsToFormat ) {
+
+                rval.append( formatNakedRange( 0, _maxElementsToFormat - ( _tailElementsToAlwaysFormat + 1 ) ) )
+                        .append( " ... " )
+                        .append( formatNakedRange( size() - _tailElementsToAlwaysFormat, size() - 1 ) );
+
+            } else {
+
+                rval.append( formatNakedRange( 0, size() - 1 ) );
+
+            }
+
+        }
+
+        rval.append( " }" );
+
+        return rval.toString();
+
+    }
+
+    public static void main( String[] args ) {
+
+        List<Integer> list = new FormattingVector<Integer>();
+
+        for ( int i = 0; i < 15; i += 1 ) {
+
+            System.out.println( "" + list );
+
+            list.add( i );
+
+        }
+
+        System.exit( 0 );
+
+    }
+
+}
